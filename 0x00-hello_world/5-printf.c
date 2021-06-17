61620a2847ca116d0dd9ef070fe6544f01ec5e89
Henock Adane <meetheni@gmail.com>
2021-06-17 12:33:34 -0700

    add 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - print the string in the printf function
+* Description: the main function is used to display a message
+* Return: 0
+*/
+
+int main(void)
+{
+printf("with proper grammer, but the outcome is a piece of art,\n");
+return (0);
+}
