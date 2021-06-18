eecb18674d68471ce3416da35d065c523d196c2d
Henock Adane <meetheni@gmail.com>
2021-06-18 13:12:34 -0700

    added 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print single digit number of base 10
+ *
+ *Description: the main function is used to display a single
+ * digit of base 10
+ *Return: 0 always
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+	printf("\n");
+	return (0);
+}
