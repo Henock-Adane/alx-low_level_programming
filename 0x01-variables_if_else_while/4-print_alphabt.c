5b7394c63fbe1324485ec102993e3989572da21f
Henock Adane <meetheni@gmail.com>
2021-06-18 13:01:19 -0700

    added 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - displays every alphabets except e and q
+*
+* Description: main function is used to print alphabets except e and q
+* Return: 0 always
+*/
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
