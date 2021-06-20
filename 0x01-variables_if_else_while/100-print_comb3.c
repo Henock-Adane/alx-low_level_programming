7855405ed6f5ae3560f3f2f051fff9424a3640d5
Henock Adane <meetheni@gmail.com>
2021-06-20 09:36:45 -0700

    added 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+*main - prints combination of two digits
+*Description: the main function is used to print digits combination
+*Return: 0
+*/
+int main(void)
+{
+	int ch = 0;
+	int ln, rn;
+
+	while (c <= 99)
+	{
+		ln = (ch / 10 + '0');
+		rn = (ch % 10 + '0');
+
+		if (ln < rn)
+		{
+			putchar(ln);
+			putchar(rn);
+
+			if (ch != 89)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
