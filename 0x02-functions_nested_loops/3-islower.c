38f83a932c25485825f484c051fe4c9966bc72ee
Henock Adane <meetheni@gmail.com>
2021-06-22 13:14:58 -0700

    modified 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,7 @@
 #include "holberton.h"
 /**
  * _islower - a function to check for lowercase character
+ * @c: is an integer parameter in the _islower function to check lowercases
  * Return: 0
  */
 int _islower(int c)
