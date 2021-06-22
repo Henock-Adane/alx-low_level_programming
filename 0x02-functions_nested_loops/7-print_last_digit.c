a41a1714ec52f1cd517c38bfa18ea3a3dde6b46d
Henock Adane <meetheni@gmail.com>
2021-06-22 14:02:45 -0700

    modified 7 again

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,14 +8,13 @@ int print_last_digit(int c)
 {
 	if (c > 0 || c == 0)
 	{
-		int d = c % 10;
-		_putchar (d + '0');
-		return (d);
+		_putchar (c % 10 + '0');
+		return (c % 10);
 	}
 	else
 	{
 		c = c * -1;
-		_putchar (d + '0');
-		return (d);
+		_putchar (c % 10 + '0');
+		return (c % 10);
 	}
 }
