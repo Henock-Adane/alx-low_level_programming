f0f4d74d929cb77286243c9eed6d91096949b11b
Henock Adane <meetheni@gmail.com>
2021-06-28 11:35:25 -0700

    added 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,27 +6,18 @@
  */
 void puts_half(char *str)
 {
-	int c, a;
+	int i, a;
 	char l;
 
-	for (c = 0; str[c] != 0; c++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
 	}
-	if (c % 2 == 0)
+	a = (i + 1) / 2;
+	for (; str[a] != 0; a++)
 	{
-		for ((a = c / 2); str[a] != 0; a++)
-		{
-			l = str[a];
-			_putchar(l);
-		}
+		l = str[a];
+		_putchar(l);
 	}
-	else
-{
-for ((a = (c - 1) / 2); str[a] != 0; a++)
-{
-l = str[a];
-_putchar(l);
-}
-}
 	_putchar('\n');
+
 }
