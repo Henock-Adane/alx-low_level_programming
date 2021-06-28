11aef5217fa46864a905e8e2321126bcfb43f466
Henock Adane <meetheni@gmail.com>
2021-06-28 10:57:34 -0700

    added 7-puts_half.c and modified holberton.h

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+/**
+ * puts_half - a function that prints half of a string, followed by a new line.
+ * @str: paramters for the function puts_half
+ * Return: Always 0.
+ */
+void puts_half(char *str)
+{
+	int c, a;
+	char l;
+
+	for (c = 0; str[c] != 0; c++)
+	{
+	}
+	if (c % 2 == 0)
+	{
+		for ((a = c / 2); str[a] != 0; a++)
+		{
+			l = str[a];
+			_putchar(l);
+		}
+	}
+	else
+{
+for ((a = (c - 1) / 2); str[a] != 0; a++)
+{
+l = str[a];
+_putchar(l);
+}
+}
+	_putchar('\n');
+}
