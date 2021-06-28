59e1977825a030735fc4bfe4f398225c9778d6a3
Henock Adane <meetheni@gmail.com>
2021-06-28 09:46:59 -0700

    added 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * _strlen - returns the length of a string.
+ *@s: the string to be checked.
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
