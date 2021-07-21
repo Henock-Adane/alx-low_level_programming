b75c2a0692b9e5596fe75c8b3ffb4b32fc65ac8f
Henock Adane <meetheni@gmail.com>
2021-07-21 11:37:04 -0700

    added 1-array_iterator.c again

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -8,10 +8,16 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	size_t i;
+	unsigned int x = 0;
 
-	for (i = 0; i < size; i++)
+	if (array != NULL && action != NULL && size > 0)
 	{
-		action(array[i]);
+
+		while (x < size)
+		{
+
+			action(array[x]);
+			x++;
+		}
 	}
 }
