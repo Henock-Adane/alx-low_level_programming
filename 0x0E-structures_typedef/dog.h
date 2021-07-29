c582001a66bf2230c24e8527e95a837a2c755d79
Henock Adane <meetheni@gmail.com>
2021-07-29 04:29:25 -0700

    added dog.h and a readme file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure definition of a dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: owner
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
