8c882f97139e83fc30dc76677fbc36723f303adc
Henock Adane <meetheni@gmail.com>
2021-07-29 06:50:24 -0700

    added 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,7 +8,7 @@ void print_dog(struct dog *d)
 {
 	if (d)
 	{
-		if (!(d->name))
+		if ((d->name) == NULL)
 			printf("Name: (nil)\n");
 			else
 				printf("Name: %s\n", d->name);
@@ -16,7 +16,7 @@ void print_dog(struct dog *d)
 			printf("Age: (nil)\n");
 			else
 				printf("Age: %f\n", d->age);
-		if (!(d->owner))
+		if ((d->owner) == NULL)
 			printf("Owner: (nil)\n");
 			else
 				printf("Owner: %s\n", d->owner);
