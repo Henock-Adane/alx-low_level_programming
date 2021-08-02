2c8d5537f2d7eb4f6ae620743e570d4c870e6215
Henock Adane <meetheni@gmail.com>
2021-08-02 01:44:50 -0700

    malloc fix

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,34 +1,29 @@
-#include "holberton.h"
 #include <stdlib.h>
-#include <stdio.h>
+#include "holberton.h"
 /**
- * *create_array - creates an array of chars
-  *and initializes it with a specific char.
- * @size: size of the array
- * @c: character to be initialized
- *Return: a pointer to the array, or NULL if it fails
+ * create_array - entry point.
+ * @size: unsigned int variable
+ * @c: char variable
+ *
+ * Return: ar or NULL
  */
+
 char *create_array(unsigned int size, char c)
 {
-	char *array;
-	unsigned int i;
+	char *ar;
+	unsigned int i = 0;
 
-	if (size != 0)
-	{
-		array = malloc(size * sizeof(char));
-	}
-	else
+	if (size == 0)
+		return (NULL);
+	ar = malloc(size);
+	if (ar == NULL)
 	{
 		return (NULL);
 	}
-
-	if (array == NULL)
-		return (NULL);
-
-	for (i = 0; i < size; i++)
+	while (i < size)
 	{
-		array[i] = c;
+		ar[i] = c;
+		i++;
 	}
-	return (array);
-
+	return (ar);
 }
--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -1,9 +1,11 @@
-#ifndef HOLBERTON
-#define HOLBERTON
+#ifndef HOLBERTON_HFILE
+#define HOLBERTON_HFILE
+
 int _putchar(char c);
-char *create_array(unsigned int size, char c)
+char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
+
 #endif
