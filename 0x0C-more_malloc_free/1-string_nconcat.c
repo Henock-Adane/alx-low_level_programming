5cdfbd773637a331bf378deaea7a2dd6124fc896
Henock Adane <meetheni@gmail.com>
2021-08-11 09:20:44 -0700

    added 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -22,7 +22,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s2 = "";
 	while (s1[k])
 		k++;
-	ar = malloc(n + k + 1);
+	ar = malloc(n + k + 1 + 5);
 	if (ar == NULL)
 		return (NULL);
 	j = 0;
